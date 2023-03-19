9cd4ae897de337f2487ca63b36d901263a638baa
emaagasta <127483790+emaagasta@users.noreply.github.com>
2023-03-20 00:22:33 +0700

    Update Driver_Option.py

--- Settup_Connection/Driver_Option.py
+++ Settup_Connection/Driver_Option.py
@@ -1,10 +1,7 @@
 
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
-from appium.webdriver.common.appiumby import AppiumBy
 
-# import sys
-# sys.path.append(sys.path[0] + "/....")
 
 class Appium_Driver_test(object):
 
